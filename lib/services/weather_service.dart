f55248ddebd7fea608e992f19c32b9d592e6e856
Omar_Ameer <omar.ameer244@gmail.com>
2023-03-16 00:30:08 +0200

    Fourth Commit

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:weather_app/models/weather_model.dart';
 
 class WeatherService {
-  void getWeather({required String cityName}) async {
+  Future<WeatherModel> getWeather({required String cityName}) async {
     String baseUrl = "http://api.weatherapi.com/v1";
     String apiKey = "6013737c20ce42b2a6e173901231503";
     Uri url =
@@ -12,7 +13,8 @@ class WeatherService {
 
     Map<String, dynamic> data = jsonDecode(response.body);
 
-    String date = data["location"]["localtime"];
-    String temp = data['forecast']['forecastday'][0]['avgtemp_c'];
+    WeatherModel weather = WeatherModel.fromJson(data);
+
+    return weather;
   }
 }
